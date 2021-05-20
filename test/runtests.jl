aed7d2c20c45c4c4cc986d42440afd2610b16e10
Josh Burton <josh@burton-online.me.uk>
2021-05-20 10:17:52 +0100

    starting to write tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,9 @@
 using DelayedKalmanFilter
-# using Test
-
-protein_at_observations = [0 100; 10 200; 20 500; 30 700; 40 400]
-model_parameters = [10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0]
-measurement_variance = 10
-
-kalman_filter(protein_at_observations,model_parameters,measurement_variance)
+using Test
 
 @testset "DelayedKalmanFilter.jl" begin
-    # tests here
+    protein_at_observations = [0 100; 10 200; 20 500; 30 700; 40 400];
+    model_parameters = [10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
+    measurement_variance = 10;
+    kalman_filter(protein_at_observations,model_parameters,measurement_variance);
 end
