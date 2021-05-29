efb4071ead0b0cee950acf382eccb1d003dd73e8
Josh Burton <josh@burton-online.me.uk>
2021-05-29 10:37:01 +0100

    adding test for negative parameter check

--- test/likelihood_tests.jl
+++ test/likelihood_tests.jl
@@ -7,6 +7,15 @@ using Test
     protein_at_observations = readdlm(string(loading_path,"kalman_filter_test_trace_observations.csv"),',');
     model_parameters = [10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
     measurement_variance = 10000;
+
+    # test negative parameter check works
+    negative_parameter_1 = [10000.0,5.0,log(2)/30,log(2)/90,-1.0,1.0,29.0];
+    negative_parameter_2 = [-10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
+
+    ll = calculate_log_likelihood_at_parameter_point(protein_at_observations,negative_parameter_1,measurement_variance);
+    @test ll == -Inf
+    ll = calculate_log_likelihood_at_parameter_point(protein_at_observations,negative_parameter_2,measurement_variance);
+    @test ll == -Inf
     # test log likelihood and derivative are correct
     ll = calculate_log_likelihood_at_parameter_point(protein_at_observations,model_parameters,measurement_variance);
     @test ll ≈ readdlm(string(loading_path,"log_likelihood_value.csv"),',')[1]
