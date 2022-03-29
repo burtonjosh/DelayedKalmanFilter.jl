4d7fdd42a5cd4a5480f267428a16b8bd2a11ffce
Josh Burton <josh@burton-online.me.uk>
2022-03-29 19:08:12 +0300

    removing unwanted dependencies

--- test/likelihood_tests.jl
+++ test/likelihood_tests.jl
@@ -5,12 +5,12 @@ using Test
 @testset "Likelihood tests" begin
     loading_path = string(pwd(),"/data/");
     protein_at_observations = readdlm(string(loading_path,"kalman_filter_test_trace_observations.csv"),',');
-    model_parameters = [10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
-    measurement_variance = 10000;
+    model_parameters = ModelParameters() #[10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
+    measurement_variance = 10000.0;
 
     # test negative parameter check works
-    negative_parameter_1 = [10000.0,5.0,log(2)/30,log(2)/90,-1.0,1.0,29.0];
-    negative_parameter_2 = [-10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
+    negative_parameter_1 = ModelParameters(10000.0,5.0,log(2)/30,log(2)/90,-1.0,1.0,29.0);
+    negative_parameter_2 = ModelParameters(-10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0);
 
     ll = calculate_log_likelihood_at_parameter_point(protein_at_observations,negative_parameter_1,measurement_variance);
     @test ll == -Inf
@@ -19,8 +19,4 @@ using Test
     # test log likelihood and derivative are correct
     ll = calculate_log_likelihood_at_parameter_point(protein_at_observations,model_parameters,measurement_variance);
     @test ll ≈ readdlm(string(loading_path,"log_likelihood_value.csv"),',')[1]
-
-    ll, lld = calculate_log_likelihood_and_derivative_at_parameter_point(protein_at_observations,model_parameters,measurement_variance);
-    @test ll ≈ readdlm(string(loading_path,"log_likelihood_value.csv"),',')[1]
-    @test lld ≈ reshape(readdlm(string(loading_path,"log_likelihood_derivative_value.csv"),','),7)
 end
