dd25b1dac68207cf8807db806df5c1bcdc970eec
burtonjosh <josh@burton-online.me.uk>
2024-02-15 22:40:03 +0000

    do some quick formatting and update CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,34 +1,23 @@
 using DelayedKalmanFilter
 using Documenter
 
-DocMeta.setdocmeta!(
-    DelayedKalmanFilter,
-    :DocTestSetup,
-    :(using DelayedKalmanFilter);
-    recursive = true,
-)
+DocMeta.setdocmeta!(DelayedKalmanFilter, :DocTestSetup, :(using DelayedKalmanFilter); recursive = true)
 
 makedocs(;
-    modules = [DelayedKalmanFilter],
-    authors = "Joshua Burton",
-    repo = "https://github.com/burtonjosh/DelayedKalmanFilter.jl/blob/{commit}{path}#L{line}",
-    sitename = "DelayedKalmanFilter.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://burtonjosh.github.io/DelayedKalmanFilter.jl",
-        assets = String[],
-    ),
-    pages = [
-        "Home" => "index.md",
-        "Examples" => [
-            "Tutorial" => "examples/tutorial.md",
-            "Parameter Estimation" => "examples/parameter-estimation.md",
-        ],
-        "Library" => ["Public" => "lib/public.md", "Internals" => "lib/internals.md"],
-    ],
+  modules = [DelayedKalmanFilter],
+  authors = "Joshua Burton",
+  repo = "https://github.com/burtonjosh/DelayedKalmanFilter.jl/blob/{commit}{path}#L{line}",
+  sitename = "DelayedKalmanFilter.jl",
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://burtonjosh.github.io/DelayedKalmanFilter.jl",
+    assets = String[],
+  ),
+  pages = [
+    "Home" => "index.md",
+    "Examples" => ["Tutorial" => "examples/tutorial.md", "Parameter Estimation" => "examples/parameter-estimation.md"],
+    "Library" => ["Public" => "lib/public.md", "Internals" => "lib/internals.md"],
+  ],
 )
 
-deploydocs(;
-    repo = "github.com/burtonjosh/DelayedKalmanFilter.jl",
-    devbranch = "continuous",
-)
+deploydocs(; repo = "github.com/burtonjosh/DelayedKalmanFilter.jl", devbranch = "main")
