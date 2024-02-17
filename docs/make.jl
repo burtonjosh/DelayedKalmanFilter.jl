abc0cdd9b823a3111b27460977a3622e196e508d
burtonjosh <josh@burton-online.me.uk>
2024-02-17 13:54:00 +0000

    add favicon and coverage

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
   format = Documenter.HTML(;
     prettyurls = get(ENV, "CI", "false") == "true",
     canonical = "https://burtonjosh.github.io/DelayedKalmanFilter.jl",
-    assets = String[],
+    assets = ["assets/favicon.ico"],
   ),
   pages = [
     "Home" => "index.md",
